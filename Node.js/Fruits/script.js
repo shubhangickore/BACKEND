2d5fc7c9f03d7026a1d0fe1b143a0e0e59840191
shubhangi kore <koreshubhangi291@gmail.com>
2025-09-10 19:42:51 +0530

    reorganized files

---
+++ Node.js/Fruits/script.js
@@ -0,0 +1,4 @@
+import {add , pi} from "../math.js";
+
+console.log(add(1,2));
+console.log(pi);
---
+++ Node.js/math.js
@@ -0,0 +1,4 @@
+export const add = (a,b) => a+b;
+export const sub = (a,b) => a-b;
+export const g = 9.8;
+export const pi = 3.14;
